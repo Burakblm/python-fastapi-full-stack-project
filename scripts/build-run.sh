0b057a2d29a1be7993313a5b9013334d49878718
BurakBlm <burakbulama5454@gmail.com>
2024-12-03 22:49:21 +0300

    enviroment variables build-run.sh file

--- scripts/build-run.sh
+++ scripts/build-run.sh
@@ -3,6 +3,12 @@ cd /home/burak/python-fastapi-full-stack-project
 
 git pull
 
+export POSTGRES_SERVER=${POSTGRES_SERVER}
+export POSTGRES_PORT=${POSTGRES_PORT}
+export POSTGRES_PASSWORD=${POSTGRES_PASSWORD}
+export POSTGRES_DB=${POSTGRES_DB}
+export POSTGRES_USER=${POSTGRES_USER}
+export ALGORITHM=${ALGORITHM}
 
 sudo docker compose -f /home/burak/python-fastapi-full-stack-project/docker-compose-prod.yml down -v
 
