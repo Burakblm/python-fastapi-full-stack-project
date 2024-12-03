1243377191fec5a25f45e07a5253b0d2af44a237
BurakBlm <burakbulama5454@gmail.com>
2024-12-03 14:21:10 +0300

    github actions configuration

---
+++ scripts/build-run.sh
@@ -0,0 +1,10 @@
+
+cd /home/burak/python-fastapi-full-stack-project
+
+git pull
+
+sudo docker compose -f /home/burak/python-fastapi-full-stack-project/docker-compose-prod.yml down -v
+
+sudo docker pull burakblm/python-full-stack-backend:latest
+
+sudo docker compose -f /home/burak/python-fastapi-full-stack-project/docker-compose-prod.yml up -d
