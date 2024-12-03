d929b7f53deda82393397e3770bc69e0c3936ae8
BurakBlm <burakbulama5454@gmail.com>
2024-12-03 19:55:47 +0300

    docker-compose command error solved

--- scripts/build-run.sh
+++ scripts/build-run.sh
@@ -3,6 +3,7 @@ cd /home/burak/python-fastapi-full-stack-project
 
 git pull
 
+
 sudo docker compose -f /home/burak/python-fastapi-full-stack-project/docker-compose-prod.yml down -v
 
 sudo docker pull burakblm/python-full-stack-backend:latest
